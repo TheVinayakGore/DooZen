18cf92e7dc5ad1911735b0535c59127626718eae
Vinayak Gore <vintech.g2628@gmail.com>
2025-03-09 19:12:23 +0530

    Update Footer.js

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <>
             <div className="container">
                 <footer className="py-3 my-4">
-                    <p className="text-center text-body-secondary">Created with ❤️ by VinTech.G</p>
+                    <p className="text-center text-body-secondary">Created with ❤️ by Vinayak Gore</p>
                     <hr />
                     <p className="text-center text-body-secondary">© 2024 DooZen, Inc</p>
                 </footer>
